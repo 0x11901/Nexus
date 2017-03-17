7800e0bc8a6287203abe61e37e05483ab8518977
王靖凯 <wjk930726@live.com>
2017-03-18 00:51:35 +0800

    add AppIcon

---
+++ Nexus/Application/Controller/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  Nexus
+//
+//  Created by 王靖凯 on 2017/3/18.
+//  Copyright © 2017年 王靖凯. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        self.setupUI()
+    }
+    
+}
+
+extension MainViewController {
+    fileprivate func setupUI() {
+        
+    }
+}
+
