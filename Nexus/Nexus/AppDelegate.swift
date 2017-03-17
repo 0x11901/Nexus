b8cf79c7c6b19d0a57d1496df0bf44d261925e4d
王靖凯 <wjk930726@live.com>
2017-03-18 00:23:14 +0800

    initial project

---
+++ Nexus/Nexus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nexus
+//
+//  Created by 王靖凯 on 2017/3/18.
+//  Copyright © 2017年 王靖凯. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Nexus/Nexus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nexus
+//
+//  Created by 王靖凯 on 2017/3/18.
+//  Copyright © 2017年 王靖凯. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
